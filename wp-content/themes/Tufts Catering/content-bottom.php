e55fddfe27364e2ad9733135bbd9bdd3bf22fc9a
kcroake <kcroake88@gmail.com>
2015-11-24 21:47:14 -0500

    Global Fields

--- wp-content/themes/Tufts Catering/content-bottom.php
+++ wp-content/themes/Tufts Catering/content-bottom.php
@@ -43,7 +43,66 @@
         elseif(get_row_layout() == "content_with_image_on_left"): 
 ?>
         
-        <?php $bgimg = get_sub_field('image'); ?>
+        <?php if(get_sub_field('pull_from_options')) { ?>
+        
+        
+		        	<?php if(get_sub_field('options_content') == 'feature1') { ?> 
+		        	
+		        	<?php $bgimg = get_field('image_1', 'options'); ?>
+		        	<section class="features-intro">
+						<div class="container-fluid">
+							<div class="row">
+					            <div class="dotted-line"></div>
+								<div class="col-md-6 nopadding features-intro-img">
+									<div class="features-bg" style="background-image: url(<?php if(isset($bgimg['sizes']['module-img'])){ echo $bgimg['sizes']['module-img']; } ?>)"></div>
+								</div>
+								<div class="col-md-6 nopadding">
+									<div class="features-text" style="background-color: #<?php the_field('content_background_color_1', 'options'); ?>;">
+					                    <div class="wow fadeInLeft center" data-wow-delay="0.1s">
+											<h1><?php the_field('headline_1', 'options'); ?></h1>
+											<?php the_field('description_1', 'options'); ?>
+											<?php if(get_field('button_link_1', 'options')): ?><p><a class="fill" href="<?php the_field('button_link_1', 'options'); ?>"><?php the_field('button_text_1', 'options'); ?></a></p><?php endif; ?>
+					                    </div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</section>
+
+		        	
+		        	
+		        	
+		        	<?php } elseif(get_sub_field('options_content')== 'feature2') { ?>
+					
+		        	<?php $bgimg = get_field('image_2', 'options'); ?>
+		        	<section class="features-intro">
+						<div class="container-fluid">
+							<div class="row">
+					            <div class="dotted-line"></div>
+								<div class="col-md-6 nopadding features-intro-img">
+									<div class="features-bg" style="background-image: url(<?php if(isset($bgimg['sizes']['module-img'])){ echo $bgimg['sizes']['module-img']; } ?>)"></div>
+								</div>
+								<div class="col-md-6 nopadding">
+									<div class="features-text" style="background-color: #<?php the_field('content_background_color_2', 'options'); ?>;">
+					                    <div class="wow fadeInLeft center" data-wow-delay="0.1s">
+											<h1><?php the_field('headline_2', 'options'); ?></h1>
+											<?php the_field('description_2', 'options'); ?>
+											<?php if(get_field('button_link_2', 'options')): ?><p><a class="fill" href="<?php the_field('button_link_2', 'options'); ?>"><?php the_field('button_text_2', 'options'); ?></a></p><?php endif; ?>
+					                    </div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</section>
+					
+					
+					<?php } ?>
+        
+        
+        
+       <?php } else { ?>
+        
+       <?php $bgimg = get_sub_field('image'); ?>
         <section class="features-intro">
 			<div class="container-fluid">
 				<div class="row">
@@ -64,9 +123,76 @@
 			</div>
 		</section>
         
+        <?php } ?>
+        
+        
+        
+
+        
 <?php 
         elseif(get_row_layout() == "content_with_image_on_right"): 
 ?>
+
+        <?php if(get_sub_field('pull_from_options')) { ?>
+        
+        
+		<?php if(get_sub_field('options_content') == 'feature1') { ?> 
+
+        <?php $bgimg = get_field('image_1','options'); ?>
+        <section class="features-intro">
+			<div class="container-fluid">
+				<div class="row">
+                    <div class="dotted-line"></div>
+					<div class="col-md-6 nopadding features-intro-img hidden-md hidden-lg">
+						<div class="features-bg" style="background-image: url(<?php if(isset($bgimg['sizes']['module-img'])){ echo $bgimg['sizes']['module-img']; } ?>)"></div>
+					</div>
+					<div class="col-md-6 nopadding">
+        				<div class="features-text" style="background-color: #<?php the_field('content_background_color_1','options'); ?>;">
+                            <div class="wow fadeInLeft center" data-wow-delay="0.1s">
+        						<h1><?php the_field('headline_1','options'); ?></h1>
+        						<?php the_field('description_1','options'); ?>
+        						<?php if(get_field('button_link_1','options')): ?><p><a class="fill" href="<?php the_field('button_link_1','options'); ?>"><?php the_field('button_text','options'); ?></a></p><?php endif; ?>
+                            </div>
+        				</div>
+					</div>
+					<div class="col-md-6 nopadding features-intro-img visible-lg visible-md hidden-sm">
+						<div class="features-bg" style="background-image: url(<?php if(isset($bgimg['sizes']['module-img'])){ echo $bgimg['sizes']['module-img']; } ?>)"></div>
+					</div>
+				</div>
+			</div>
+		</section>					
+
+		<?php } elseif(get_sub_field('options_content')== 'feature2') { ?>
+				  	
+        <?php $bgimg = get_field('image_2','options'); ?>
+        <section class="features-intro">
+			<div class="container-fluid">
+				<div class="row">
+                    <div class="dotted-line"></div>
+					<div class="col-md-6 nopadding features-intro-img hidden-md hidden-lg">
+						<div class="features-bg" style="background-image: url(<?php if(isset($bgimg['sizes']['module-img'])){ echo $bgimg['sizes']['module-img']; } ?>)"></div>
+					</div>
+					<div class="col-md-6 nopadding">
+        				<div class="features-text" style="background-color: #<?php the_field('content_background_color_2','options'); ?>;">
+                            <div class="wow fadeInLeft center" data-wow-delay="0.1s">
+        						<h1><?php the_field('headline_2','options'); ?></h1>
+        						<?php the_field('description_2','options'); ?>
+        						<?php if(get_field('button_link_2','options')): ?><p><a class="fill" href="<?php the_field('button_link_2','options'); ?>"><?php the_sub_field('button_text_2','options'); ?></a></p><?php endif; ?>
+                            </div>
+        				</div>
+					</div>
+					<div class="col-md-6 nopadding features-intro-img visible-lg visible-md hidden-sm">
+						<div class="features-bg" style="background-image: url(<?php if(isset($bgimg['sizes']['module-img'])){ echo $bgimg['sizes']['module-img']; } ?>)"></div>
+					</div>
+				</div>
+			</div>
+		</section>
+		
+						  	
+				  	<?php } ?>
+				  	
+				  	
+		  <?php } else { ?>		  	
         
         <?php $bgimg = get_sub_field('image'); ?>
         <section class="features-intro">
@@ -91,6 +217,8 @@
 				</div>
 			</div>
 		</section>
+		
+		<?php } ?>
 
 
 <?php 
